567632af8440e668d18c7baaaf90c20ff670c49e
Audiotuete <jan.moll@protonmail.com>
2023-11-02 19:12:23 +0000

    refactor: Box holds styles on root element This allows styling with flexbox.

--- src/components/box/box-zero.tsx
+++ src/components/box/box-zero.tsx
@@ -1,12 +1,13 @@
-import { Component, Prop, State, h } from '@stencil/core'
+import { Component, Prop, State, Element, h } from '@stencil/core'
 
 @Component({
   tag: 'box-zero',
   styleUrl: 'box-zero.css',
   scoped: true,
 })
-export class ZeroBox {
-  @Prop() as: 'header' | 'footer' | 'main' | 'article' | 'section' | 'aside'
+export class Box {
+  @Prop() name?: string
+  @Prop() as?: 'header' | 'footer' | 'main' | 'article' | 'section' | 'aside'
   @Prop() h?: string
   @Prop() w?: string
   @Prop() m?: string
@@ -19,21 +20,37 @@ export class ZeroBox {
   @Prop() pb?: string
   @Prop() pr?: string
   @Prop() pl?: string
+  @Prop() flex?: string
   @Prop() row?: boolean
-  @Prop() reverse?: boolean
-  @Prop() wrap?: boolean
+  @Prop() block?: boolean
   @Prop() justify?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly'
   @Prop() align?: 'center' | 'flex-start' | 'flex-end'
-  @Prop() flex?: string
-  @Prop() name?: string
+  @Prop() gap?: string
+  @Prop() rowGap?: string
+  @Prop() columnGap?: string
+  @Prop() wrap?: boolean
+  @Prop() border?: string
+  @Prop() borderWidth?: string
+  @Prop() borderStyle?: string
+  @Prop() borderColor?: string
+  @Prop() borderRadius?: string
+  @Prop() cursor?: string
+  @Prop() pointerEvents?: string
+  @Prop() zIndex?: string
+  @Prop() boxShadow?: string
+  @Prop() background?: string
 
   @State() colorfull: string
 
-  componentDidLoad() {
-    this.enableColorful()
+  @Element() el: HTMLElement
+
+  componentWillLoad() {
+    this.addStylesToRoot()
+    if (this.el.hasAttribute('name')) {
+      this.enableColorful()
+    }
   }
 
-  box!: HTMLInputElement
   enableColorful = () => {
     const boxLevelColors = {
       _1: '#6876FC',
@@ -47,17 +64,15 @@ export class ZeroBox {
       _9: '#9A1799',
       _10: '#595959',
     }
-    this.colorfull = boxLevelColors[this.getParentBoxesCount(this.box)]
-    console.log(this.colorfull)
+    this.colorfull = boxLevelColors[this.getParentBoxesCount(this.el)]
+    this.el.style['borderColor'] = this.colorfull
   }
 
-  counter: number = 0
+  counter = 0
   getParentBoxesCount: any = (el: HTMLElement) => {
-    if (el.parentElement!.tagName === 'BODY') {
-      console.log(this.counter)
-
+    if (el.tagName === 'BODY') {
       return `_${this.counter}`
-    } else if (el.classList.contains('box')) {
+    } else if (el.tagName === 'BOX-ZERO' && el.hasAttribute('name')) {
       this.counter++
       return this.getParentBoxesCount(el.parentElement)
     } else {
@@ -65,43 +80,56 @@ export class ZeroBox {
     }
   }
 
-  render() {
-    const Comp = this.as ? this.as : 'div'
+  addStylesToRoot() {
+    let styles = {
+      height: this.h,
+      width: this.w,
+      maxHeight: this.h,
+      maxWidth: this.w,
+      margin: this.m && this.m,
+      marginTop: !this.mt ? this.m : this.mt,
+      marginBottom: !this.mb ? this.m : this.mb,
+      marginRight: !this.mr ? this.m : this.mr,
+      marginLeft: !this.ml ? this.m : this.ml,
+      padding: this.p,
+      paddingTop: !this.pt ? this.p : this.pt,
+      paddingBottom: !this.pb ? this.p : this.pb,
+      paddingRight: !this.pr ? this.p : this.pr,
+      paddingLeft: !this.pl ? this.p : this.pl,
+      display: this.block && 'block',
+      flex: this.flex,
+      flexDirection: this.row ? 'row' : 'column',
+      justifyContent: this.justify,
+      alignItems: this.align,
+      gap: this.gap,
+      rowGap: this.rowGap,
+      columnGap: this.columnGap,
+      flexWrap: this.wrap && 'wrap',
+      borderWidth: this.borderWidth,
+      borderStyle: this.borderStyle,
+      borderColor: this.colorfull ? this.colorfull : this.borderColor,
+      borderRadius: this.borderRadius,
+      cursor: this.cursor,
+      pointerEvents: this.pointerEvents,
+      zIndex: this.zIndex,
+      boxShadow: this.boxShadow,
+      backgroundColor: this.background,
+    }
+    for (const property in styles) {
+      this.el.style[property] = styles[property]
+    }
+  }
 
-    return (
-      <Comp
-        ref={el => (this.box = el as HTMLInputElement)}
-        class="box"
-        style={{
-          borderColor: this.colorfull,
-          flexDirection: this.row ? 'row' : 'column',
-          justifyContent: this.justify,
-          alignItems: this.align,
-          flexWrap: this.wrap && 'wrap',
-          flex: this.flex,
-          margin: this.m && this.m,
-          marginTop: !this.mt ? this.m : this.mt,
-          marginBottom: !this.mb ? this.m : this.mb,
-          marginRight: !this.mr ? this.m : this.mr,
-          marginLeft: !this.ml ? this.m : this.ml,
-          padding: this.p,
-          paddingTop: !this.pt ? this.p : this.pt,
-          paddingBottom: !this.pb ? this.p : this.pb,
-          paddingRight: !this.pr ? this.p : this.pr,
-          paddingLeft: !this.pl ? this.p : this.pl,
-          maxHeight: this.h,
-          maxWidth: this.w,
-          height: this.h,
-          width: this.w,
-        }}
-      >
-        <slot />
-        {this.name && this.colorfull && (
-          <span style={{ color: this.colorfull }} class="name">
-            {this.name}
-          </span>
-        )}
-      </Comp>
-    )
+  render() {
+    if (this.name && this.colorfull) {
+      return [
+        <slot />,
+        <span class="box-name" style={{ color: this.colorfull }}>
+          {this.name}
+        </span>,
+      ]
+    } else {
+      return <slot />
+    }
   }
 }
